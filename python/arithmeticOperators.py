3f9aa3143545d08b95fdc41a83a70ef83339b006
_ghost <abijiththampi40@gmail.com>
2025-09-10 15:21:17 +0530

    Create arithmeticOperators.py
    basic arithmetic operators using python

---
+++ python/arithmeticOperators.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number "))
+b=int(input("Enter the second number "))
+
+print("Additon ",a+b)
+print("Subtraction ",a-b)
+print("Division ",a/b if b!=0 else "You serious??")
+print("Multiplication ",a*b)
