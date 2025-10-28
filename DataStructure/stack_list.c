025b2731f999442393a9a646273b74ebfc6e9763
_ghost <abijiththampi40@gmail.com>
2025-10-28 14:22:32 +0530

    Create stack_list.c

---
+++ DataStructure/stack_list.c
@@ -0,0 +1,106 @@
+//Stack implementation using list
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+int data;
+struct Node* next;
+};
+
+struct Node* top=NULL;
+
+
+//push a value to the stack
+void push(int value){
+struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
+if (newNode==NULL){
+printf("Stack Overflow !\n");
+return;
+}
+newNode->data=value;
+newNode->next=top;
+top=newNode;
+printf("%d Pushed to stack \n ",value);
+}
+
+
+//pop a value from the stack
+void pop(){
+if(top==NULL){
+printf("Stack Underflow !\n");
+return;
+}
+struct Node* temp=top;
+printf("%d popped from stack \n",top->data);
+top=top->next;
+free(temp);
+}
+
+
+//to see the top element
+void peek(){
+if(top==NULL){
+printf("Stack is empty !\n");
+return;
+}
+printf("Top element is %d \n ",top->data);
+}
+
+
+//to display all the elements in the stack
+void display(){
+if(top==NULL){
+printf("Stack is empty !\n");
+return;
+}
+struct Node* temp=top;
+printf("Stack elements are :\n");
+while(temp!=NULL){
+printf("%d\n",temp->data);
+temp=temp->next;
+}
+}
+
+
+//main function
+int main(){
+int ch,value;
+while(1){
+printf("\n--Stack using list--\n");
+printf("1.Push\n");
+printf("2.Pop\n");
+printf("3.Peek\n");
+printf("4.Display\n");
+printf("5.Exit\n");
+printf("Enter your choice ");
+scanf("%d",&ch);
+switch(ch){
+case 1:
+printf("Enter value to push: ");
+scanf("%d",&value);
+push(value);
+break;
+case 2:
+pop();
+break;
+case 3:
+peek();
+break;
+case 4:
+display();
+break;
+case 5:
+printf("Exiting..\n");
+exit(0);
+default:
+printf("Invalid choice..\n");
+}
+}
+return 0;
+}
+
+
+
+
+
+
