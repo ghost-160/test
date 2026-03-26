971c906d9611503d7b8df27b66e2d37b115213d2
_ghost <abijiththampi40@gmail.com>
2026-03-26 01:59:31 -0700

    Create Square.java

---
+++ Java/graphics/Square.java
@@ -0,0 +1,13 @@
+package graphics;
+
+public class Square {
+    double s;
+
+    public Square(double s) {
+        this.s = s;
+    }
+
+    public double area() {
+        return s * s;
+    }
+}
